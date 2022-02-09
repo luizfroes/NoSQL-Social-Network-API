aefdb64d0577108dbb57cc4410abf0247f207a28
Luiz Carlos Froes <luizfroes@gmail.com>
2022-02-09 18:43:05 +0000

    Added a mockdata

---
+++ src/seeds/data/thoughts.js
@@ -0,0 +1,233 @@
+const thoughts = [
+  {
+    thoughtText: "NoSQL is super easy",
+    username: "bobsmith",
+    reactions: [
+      {
+        reactionId: "123",
+        reactionBody: "Yes I agree with you!!",
+        username: "janesmith",
+      },
+      {
+        reactionId: "456",
+        reactionBody: "Yup agree",
+        username: "tomsmith",
+      },
+      {
+        reactionId: "789",
+        reactionBody: "I am struggling with NoSQL",
+        username: "jacksmith",
+      },
+    ],
+  },
+  {
+    thoughtText: "NoSQL is super easy",
+    username: "bobsmith",
+    reactions: [
+      {
+        reactionId: "123",
+        reactionBody: "Yes I agree with you!!",
+        username: "jackswift",
+      },
+      {
+        reactionId: "456",
+        reactionBody: "Yup agree",
+        username: "carolbaracat",
+      },
+      {
+        reactionId: "789",
+        reactionBody: "I am struggling with NoSQL",
+        username: "gracekelly",
+      },
+    ],
+  },
+  {
+    thoughtText: "NoSQL is killing me",
+    username: "gracekelly",
+    reactions: [
+      {
+        reactionId: "123",
+        reactionBody: "Yes, it is!!",
+        username: "tonyjunior",
+      },
+      {
+        reactionId: "456",
+        reactionBody: "Really??",
+        username: "carolbaracat",
+      },
+      {
+        reactionId: "789",
+        reactionBody: "I don't think so!!",
+        username: "bobsmith",
+      },
+    ],
+  },
+  {
+    thoughtText: "I love churros!!",
+    username: "bobsmith",
+    reactions: [
+      {
+        reactionId: "123",
+        reactionBody: "Me too!!",
+        username: "jackswift",
+      },
+      {
+        reactionId: "456",
+        reactionBody: "I just hate them!!",
+        username: "carolbaracat",
+      },
+      {
+        reactionId: "789",
+        reactionBody: "Best desert ever!!",
+        username: "tonyjunior",
+      },
+    ],
+  },
+  {
+    thoughtText: "I am so tired today...",
+    username: "gracekelly",
+    reactions: [
+      {
+        reactionId: "123",
+        reactionBody: "You need to rest!!",
+        username: "carolbaracat",
+      },
+      {
+        reactionId: "456",
+        reactionBody: "Why??",
+        username: "bobsmith",
+      },
+      {
+        reactionId: "789",
+        reactionBody: "I am missing my bed too!!",
+        username: "jackswift",
+      },
+    ],
+  },
+  {
+    thoughtText: "I hate stupid people!!",
+    username: "jackswift",
+    reactions: [
+      {
+        reactionId: "123",
+        reactionBody: "Take easy!!!!",
+        username: "jackswift",
+      },
+      {
+        reactionId: "456",
+        reactionBody: "What happened?",
+        username: "gracekelly",
+      },
+      {
+        reactionId: "789",
+        reactionBody: "Hope you are fine!!",
+        username: "tonyjunior",
+      },
+    ],
+  },
+  {
+    thoughtText: "It's Friday night!!!",
+    username: "jackswift",
+    reactions: [
+      {
+        reactionId: "123",
+        reactionBody: "Where are we going to?",
+        username: "carolbaracat",
+      },
+      {
+        reactionId: "456",
+        reactionBody: "Yup I love Fridays",
+        username: "tonyjunior",
+      },
+      {
+        reactionId: "789",
+        reactionBody: "I will work till late today...",
+        username: "bobsmith",
+      },
+    ],
+  },
+  {
+    thoughtText: "I am very happy today!!",
+    username: "carolbaracat",
+    reactions: [
+      {
+        reactionId: "123",
+        reactionBody: "Yuhuu!!",
+        username: "bobsmith",
+      },
+      {
+        reactionId: "456",
+        reactionBody: "Enjoy your moment!!",
+        username: "tonyjunior",
+      },
+      {
+        reactionId: "789",
+        reactionBody: "That's great!!",
+        username: "gracekelly",
+      },
+    ],
+  },
+  {
+    thoughtText: "I just don't know what to do...",
+    username: "carolbaracat",
+    reactions: [
+      {
+        reactionId: "123",
+        reactionBody: "Get a churros!!",
+        username: "bobsmith",
+      },
+      {
+        reactionId: "456",
+        reactionBody: "I am here if you need!!",
+        username: "jackswift",
+      },
+      {
+        reactionId: "789",
+        reactionBody: "I am struggling with my homework",
+        username: "carolbaracat",
+      },
+    ],
+  },
+  {
+    thoughtText: "Today is my birthday!!!",
+    username: "carolbaracat",
+    reactions: [
+      {
+        reactionId: "123",
+        reactionBody: "Happy birthday sweet!!!!!",
+        username: "jackswift",
+      },
+      {
+        reactionId: "456",
+        reactionBody: "All the best!!",
+        username: "tonyjunior",
+      },
+      {
+        reactionId: "789",
+        reactionBody: "Let's celebrate!!!!",
+        username: "gracekelly",
+      },
+    ],
+  },
+  {
+    thoughtText: "I've been promoted!!!",
+    username: "tonyjunior",
+    reactions: [
+      {
+        reactionId: "123",
+        reactionBody: "Let's party!!!!!",
+        username: "bobsmith",
+      },
+      {
+        reactionId: "456",
+        reactionBody: "So, are you paying the drinks?",
+        username: "jackswift",
+      },
+      {
+        reactionId: "789",
+        reactionBody: "I am very happy for you!!",
+        username: "gracekelly",
+      },
+    ],
+  },
+];
---
+++ src/seeds/data/users.js
@@ -0,0 +1,22 @@
+const users = [
+  {
+    username: "bobsmith",
+    email: "bobsmith@email.com",
+  },
+  {
+    username: "jackswift",
+    email: "jackswift@email.com",
+  },
+  {
+    username: "carolbaracat",
+    email: "carolbaracat@email.com",
+  },
+  {
+    username: "gracekelly",
+    email: "gracekelly@email.com",
+  },
+  {
+    username: "tonyjunior",
+    email: "tonyjunior@email.com",
+  },
+];
