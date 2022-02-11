c2897629dc22c0d267dfb90e118e5208d004e433
Luiz Carlos Froes <luizfroes@gmail.com>
2022-02-11 00:57:35 +0000

    Added users controllers

--- src/controllers/api/users.js
+++ src/controllers/api/users.js
@@ -1,21 +1,66 @@
-const getUsers = (req, res) => {
-  res.send("getUsers");
+const { User } = require("../../models");
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find({}).populate("thoughts");
+
+    res.json({ success: true, data: users });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get users | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to get users" });
+  }
 };
 
-const getUserById = (req, res) => {
-  res.send("getUserById");
+const getUserById = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const user = await User.findById(userId).populate("thoughts");
+
+    res.json({ success: true, data: user });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create user | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to create user" });
+  }
 };
 
-const createUser = (req, res) => {
-  res.send("createUser");
+const createUser = async (req, res) => {
+  try {
+    const { username, email } = req.body;
+
+    const user = await User.create({ username, email });
+
+    res.json({ success: true, data: user });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get user | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to get user" });
+  }
 };
 
 const updateUserById = (req, res) => {
+  const { userId } = req.params;
   res.send("updateUserById");
 };
 
-const deleteUserById = (req, res) => {
-  res.send("deleteUserById");
+const deleteUserById = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const user = await User.findByIdAndDelete(userId);
+
+    res.json({ success: true, data: user });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to delete user | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to delete user" });
+  }
 };
 
 module.exports = {
